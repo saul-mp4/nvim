4fec2f313e01b66021fd6d8942596b2f0207c818
saul-mp4 <mamyrov10sultan@gmail.com>
2024-06-26 12:19:09 +0600

    module plugins

--- init.lua
+++ init.lua
@@ -13,32 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-vim.cmd.colorscheme("catppuccin")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "javascript", "html" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-})
-
+require("lazy").setup("plugins")
